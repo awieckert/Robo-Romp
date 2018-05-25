6bfc03640c493e4abc4f949a0910f8a6e9d27c3d
Adam Wieckert <wiec1369@gmail.com>
2018-05-25 15:26:39 -0500

    I Have the attack animations working. Optimization for preferences can be made.

--- javascripts/events.js
+++ javascripts/events.js
@@ -9,28 +9,28 @@ let fightStatus = 'Off';
 
 $('#player1').mouseover(function () {
   if (fightStatus === 'Off') {
-    $(this).find('.robot').fadeOut(500, function () {
-      $('.stats1').fadeIn();
+    $(this).find('.robot').fadeOut(300, function () {
+      $('.stats1').fadeIn(300);
     });
   }
 }).mouseleave(function () {
   if (fightStatus === 'Off') {
-    $(this).find('.stats1').fadeOut(500, function () {
-      $('.robot').fadeIn();
+    $(this).find('.stats1').fadeOut(300, function () {
+      $('.robot').fadeIn(300);
     });
   }
 });
 
 $('#player2').mouseover(function () {
   if (fightStatus === 'Off') {
-    $(this).find('.robot').fadeOut(500, function () {
-      $('.stats2').fadeIn();
+    $(this).find('.robot').fadeOut(300, function () {
+      $('.stats2').fadeIn(300);
     });
   }
 }).mouseleave(function () {
   if (fightStatus === 'Off') {
-    $(this).find('.stats2').fadeOut(500, function () {
-      $('.robot').fadeIn();
+    $(this).find('.stats2').fadeOut(300, function () {
+      $('.robot').fadeIn(300);
     });
   }
 });
@@ -58,14 +58,40 @@ const addAttackEvent = () => {
   $('body').on('click', '.attack', playerAttack);
 };
 
+const playerHit = (num) => {
+  window.setTimeout(function () { $(`#player${num}`).addClass('animated shake'); }, 250);
+};
+
+const removeHit = (num) => {
+  window.setTimeout(function () { $(`#player${num}`).removeClass('animated shake'); }, 1000);
+};
+
+const attackAnimations1 = () => {
+  $('#player2').removeClass('animated slideInRight');
+  $('#attack1').prop('disabled', true);
+  $('#attack2').prop('disabled', false);
+  $('#player1').addClass('animated slideInLeft');
+  $('#player2').removeClass('animated slideInRight');
+  playerHit(2);
+  removeHit(2);
+};
+
+const attackAnimations2 = () => {
+  $('#attack1').prop('disabled', false);
+  $('#attack2').prop('disabled', true);
+  $('#player2').addClass('animated slideInRight');
+  $('#player1').removeClass('animated slideInLeft');
+  playerHit(1);
+  removeHit(1);
+};
+
 const playerAttack = (e) => {
   const player1 = data.getPlayer1Bot();
   const player2 = data.getPlayer2Bot();
   // const attackingPlayer = $(e.target).closest('.robot').prop('id');
   switch (playerTurn) {
     case 1:
-      $('#attack1').prop('disabled', true);
-      $('#attack2').prop('disabled', false);
+      attackAnimations1();
       const player1Attack = player1.swing();
       let damageDealt = (player1Attack - player2.armor);
       damageDealt = damageDealt.toFixed(1);
@@ -76,12 +102,10 @@ const playerAttack = (e) => {
       if (player2.health <= 0) {
         dom.printWinner(player1);
       }
-      console.log('player1 dmg: ', damageDealt);
       playerTurn = 2;
       break;
     case 2:
-      $('#attack1').prop('disabled', false);
-      $('#attack2').prop('disabled', true);
+      attackAnimations2();
       const player2Attack = player2.swing();
       let damageDealt2 = (player2Attack - player1.armor);
       damageDealt2 = damageDealt2.toFixed(1);
@@ -92,7 +116,6 @@ const playerAttack = (e) => {
       if (player1.health <= 0) {
         dom.printWinner(player2);
       }
-      console.log('Player2 Dmg: ', damageDealt2);
       playerTurn = 1;
       break;
   }
